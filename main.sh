c55a539ae348cfd0b764c0fd5c1198d228d34fe2
119780 <119780@newington.nsw.edu.au>
2025-05-31 13:36:59 +1000

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+src=$(dirname "$0")
+
+parser() {
+	local parsed=""
+	
+	local pline=""
+	local in_func=0
+	
+	while IFS= read -r line; do
+		if [[ $in_func == 0 && $line =~ ^@([a-zA-Z_][a-zA-Z0-9_]*)[[:space:]]+(.*)\{$ ]]; then
+			in_func=1
+			pline="${BASH_REMATCH[1]}() {"
+			local args=(${BASH_REMATCH[2]})
+			for i in "${!args[@]}"; do
+				pline+=" local ${args[i]}=\"\$$(($i+1))\";"
+			done
+		elif [[ $line =~ ^\:([a-zA-Z_][a-zA-Z0-9_]*)[[:space:]]*(.*)$ ]]; then
+			pline="${BASH_REMATCH[1]} ${BASH_REMATCH[2]}"
+		else
+			if [[ $in_func == 1 && $line =~ ^\}$ ]]; then
+				in_func=0
+			fi
+			pline="$line"
+		fi
+		
+		parsed+="$pline"$'\n'
+	done
+	bash <<< "$parsed"
+}
+
+parse() {
+  	awk -v lib="$(basename "${BASH_SOURCE[0]}")" '!($0 ~ "^source .*" lib "$")' "${BASH_SOURCE[2]}" | parser
+  	exit 0
+}
+
+if [[ "${BASH_SOURCE[0]}" != "$0" ]]; then
+	parse
+fi
